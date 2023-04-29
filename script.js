b809134cd2f8ad525edf7d25043008a7692eded8
unknown <KacperPR@interia.pl>
2023-04-29 20:46:27 +0200

    Dodanie zdjęć zawodników

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ function Hamburger(x)
 {
     x.classList.toggle("change");
 }
+
+jQuery(function($) 
+    {
+        //zresetuj scrolla
+        $.scrollTo(0);
+
+        $('.scrollup').click(function() {$.scrollTo($('body'), 1000);});
+    }
+    );
+
+    //pokaż podczas przewijania
+	$(window).scroll(function()
+	{
+		if($(this).scrollTop()>800) $('.scrollup').fadeIn();
+		else $('.scrollup').fadeOut();		
+	}
+	);
